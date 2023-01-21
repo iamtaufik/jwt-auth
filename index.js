f7b8419533d961402f687416192411c0678a527e
muhammadtaufikhidayat <muhtopik07@gmail.com>
2023-01-21 16:30:44 +0700

    change origin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://auth-testing-ten.vercel.app',
   })
 );
 app.use(express.urlencoded({ extended: false }));
