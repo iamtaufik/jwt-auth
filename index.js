29aa445edf4bfd52d8acc109e64fc79d0e9ba852
muhammadtaufikhidayat <muhtopik07@gmail.com>
2023-01-21 16:09:31 +0700

    add Access-Control-Allow-Origin

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: '*',
+    origin: 'http://127.0.0.1:5173',
   })
 );
 app.use(express.urlencoded({ extended: false }));
